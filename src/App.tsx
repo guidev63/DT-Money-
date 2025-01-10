886dedf21fbb9e3acec7779c1a9417f8314da140
Guilherme Augusto <guilhermedev23@gmail.com>
2025-01-10 17:56:04 -0300

    ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { ThemeProvider } from "styled-components";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
- export function App() {
-
+export function App() {
   return (
-    <div>
-      <h1>Hello Word</h1>
-    </div>
-  )
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+
+        <h1>Hello World</h1>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
