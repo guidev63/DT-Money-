e32379dfd50013a00cc5ee848bd000d28d56b086
Guilherme Augusto <guilhermedev23@gmail.com>
2025-01-21 22:08:14 -0300

    ok

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "../components/SearchForm";
@@ -7,7 +8,30 @@ import {
   TransitionsTable,
 } from "./styles";
 
+
+
+interface Transactions{
+id:number;
+description:string;
+type:'income' | 'outcome';
+price:number;
+category:string;
+createdAt:string;
+}
 export function Transactions() {
+
+  const [transactions,setTransactions] = useState<Transactions[]>([])
+  async function loadTransactions() {
+    const response = await fetch('http://localhost:3333/transactions');
+    const data = await response.json();
+    setTransactions(data);
+  }
+
+  useEffect(() => {
+   
+    loadTransactions();
+  }, []);
+
   return (
     <div>
       <Header />
@@ -16,26 +40,22 @@ export function Transactions() {
         <SearchForm />
         <TransitionsTable>
           <tbody>
-            <tr>
-              <td>Desenvolvimento de site</td>
-              <td>
-                <PriceHighlight variant="income">
-                  R$12.000,00
-                </PriceHighlight>
-              </td>
-              <td>Vendas</td>
-              <td>13/04/2022</td>
-            </tr>
-            <tr>
-              <td>Hamburger</td>
-              <td>
-                <PriceHighlight variant="outcome">
-                  - R$ 50,00
-                </PriceHighlight>
-              </td>
-              <td>Alimentação</td>
-              <td>10/04/2022</td>
-            </tr>
+            {transactions.map(transaction =>{
+              return(
+                <tr key={transaction.id}>
+                <td width="50">{transaction.description}</td>
+                <td>
+                  <PriceHighlight variant={transaction.type}>
+                  {transaction.price}
+                  </PriceHighlight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{transaction.createdAt}</td>
+              </tr>
+
+
+              )
+            })}
           </tbody>
         </TransitionsTable>
       </TransitionsContainer>
