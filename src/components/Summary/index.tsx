38d7de8145b436b0ee1779e8a44153f33d73cf7d
Guilherme Augusto <guilhermedev23@gmail.com>
2025-01-26 12:53:01 -0300

    ok

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -1,56 +1,54 @@
 import { useContext } from "react";
 import { SummaryCard, SummaryContainer } from "./styles";
-import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from "phosphor-react";
 import { TransactionsContext } from "../../contexts/TransactionsContext";
 
 export function Summary() {
-    const { transactions } = useContext(TransactionsContext);
+  const { transactions } = useContext(TransactionsContext);
 
-    const summary = transactions.reduce(
-        (acc, transaction) => {
-            if (transaction.type === 'income') {
-                
-                acc.income += transaction.price; // Adicionado o resultado à propriedade income
-                acc.total  += transaction.price;
-            } else {
+  const summary = transactions.reduce(
+    (acc, transaction) => {
+      if (transaction.type === "income") {
+        acc.income += transaction.price;
+      } else {
+        acc.outcome += transaction.price;
+      }
 
-                acc.outcome += transaction.price;
-                acc.total -= transaction.price;
-            }
+      // Total como soma de income e outcome
+      acc.total = acc.income - acc.outcome;
 
-            return acc;
-        },
-        {
-            income: 0,
-            outcome: 0,
-            total: 0
-        }
-    );
+      return acc;
+    },
+    {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    }
+  );
 
-    return (
-        <SummaryContainer>
-            <SummaryCard>
-                <header>
-                    <span>Entradas</span>
-                    <ArrowCircleUp size={32} color="#00b37e" />
-                </header>
-                <strong>{summary.income}</strong> {/* Agora mostrará 1400 para income */}
-            </SummaryCard>
-            <SummaryCard>
-                <header>
-                    <span>Saídas</span>
-                    <ArrowCircleDown size={32} color="#f75a68" />
-                </header>
-                <strong>{summary.outcome}</strong>
-            </SummaryCard>
-
-            <SummaryCard variant="green">
-                <header>
-                    <span>Total</span>
-                    <CurrencyDollar size={32} color="#fff" />
-                </header>
-                <strong>{summary.total}</strong>
-            </SummaryCard>
-        </SummaryContainer>
-    );
+  return (
+    <SummaryContainer>
+      <SummaryCard>
+        <header>
+          <span>Entradas</span>
+          <ArrowCircleUp size={32} color="#00b37e" />
+        </header>
+        <strong>{summary.income}</strong> {/* Valor das entradas */}
+      </SummaryCard>
+      <SummaryCard>
+        <header>
+          <span>Saídas</span>
+          <ArrowCircleDown size={32} color="#f75a68" />
+        </header>
+        <strong>{summary.outcome}</strong> {/* Valor das saídas */}
+      </SummaryCard>
+      <SummaryCard variant="green">
+        <header>
+          <span>Total</span>
+          <CurrencyDollar size={32} color="#fff" />
+        </header>
+        <strong>{summary.total}</strong> {/* Valor do total */}
+      </SummaryCard>
+    </SummaryContainer>
+  );
 }
